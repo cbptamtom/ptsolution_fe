de46f691cc8f5f39fb821c1beb48cfc48ca66a1b
cbptamtom <cbptamcode@gmail.com>
2024-01-06 22:17:07 +0700

    upload fragm and ifc file

--- app/OpenComponentService/index.ts
+++ app/OpenComponentService/index.ts
@@ -1,7 +1,9 @@
 import * as THREE from "three";
 import * as OBC from "openbim-components";
 import { RefObject } from "react";
-import * as dat from "dat.gui";
+import * as path from "path";
+import * as WEBIFC from "web-ifc";
+
 const openComponent = async (containerRef: RefObject<HTMLDivElement> | null) => {
 	if (containerRef && containerRef.current) {
 		const container = containerRef.current;
@@ -11,7 +13,6 @@ const openComponent = async (containerRef: RefObject<HTMLDivElement> | null) => 
 			components.renderer = new OBC.SimpleRenderer(components, container);
 			components.camera = new OBC.SimpleCamera(components);
 			components.raycaster = new OBC.SimpleRaycaster(components);
-			console.log("object");
 			components.init();
 			const scene = components.scene.get();
 			// components.camera.controls.setLookAt(10, 10, 10, 0, 0, 0);
@@ -26,47 +27,116 @@ const openComponent = async (containerRef: RefObject<HTMLDivElement> | null) => 
 			ambientLight.intensity = 0.5;
 			scene.add(ambientLight);
 
-			const fragments = new OBC.FragmentManager(components);
+			let fragments = new OBC.FragmentManager(components);
+			let fragmentIfcLoader = new OBC.FragmentIfcLoader(components);
 
-			const file = await fetch("../../resources/small.frag");
-			const data = await file.arrayBuffer();
-			const buffer = new Uint8Array(data);
-			const model = await fragments.load(buffer);
-			const meshes = [];
-			const culler = new OBC.ScreenCuller(components);
-			for (const fragment of model.items) {
-				meshes.push(fragment.mesh);
-				culler.add(fragment.mesh);
-			}
-			culler.needsUpdate = true;
+			const toolbar = new OBC.Toolbar(components);
+			components.ui.addToolbar(toolbar);
+			toolbar.addChild(fragments.uiElement.get("main"));
 
-			const cubeGeometry = new THREE.BoxGeometry(3, 3, 3);
-			const cubeMaterial = new THREE.MeshStandardMaterial({ color: "#C0EBA5" });
-			const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-			cube.position.set(0, 1.5, 0);
+			const loadFragments = async () => {
+				if (fragments.groups.length) return;
+				// const file = await fetch("./../../resources/small.frag");
+				const file = await fetch("small.frag");
+				console.log(file);
+				const data = await file.arrayBuffer();
+				const buffer = new Uint8Array(data);
+				fragments.load(buffer);
+				// const scene = components.scene.get();
+				// scene.add(model);
+			};
+			const loadButton = new OBC.Button(components);
+			loadButton.materialIcon = "upload model";
+			toolbar.addChild(loadButton);
+			loadButton.onClick.add(async () => {
+				console.log("load Fragment");
+				await loadFragments();
+			});
 
-			scene.add(cube);
-			components.meshes.push(cube);
-
-			const shadows = new OBC.ShadowDropper(components);
-			shadows.shadowExtraScaleFactor = 15;
-			shadows.darkness = 2;
-			shadows.shadowOffset = 0.1;
-			shadows.renderShadow([cube], "example");
-
-			const dimensions = new OBC.LengthMeasurement(components);
-			dimensions.enabled = true;
-			dimensions.snapDistance = 1;
-			container.ondblclick = () => dimensions.create();
-			window.onkeydown = (event) => {
-				if (event.code === "Delete" || event.code === "Backspace") {
-					dimensions.delete();
-				}
+			const exportFragments = () => {
+				if (!fragments.groups.length) return;
+				const group = fragments.groups[0];
+				const data = fragments.export(group);
+				const blob = new Blob([data]);
+				const file = new File([blob], "small.frag");
+				download(file);
 			};
 
+			const download = (file: any) => {
+				const link = document.createElement("a");
+				link.href = URL.createObjectURL(file);
+				link.download = file.name;
+				document.body.appendChild(link);
+				link.click();
+				link.remove();
+			};
+			const exportButton = new OBC.Button(components);
+			exportButton.materialIcon = "download model";
+			toolbar.addChild(exportButton);
+			exportButton.onClick.add(() => exportFragments());
+
+			const disposeFragments = () => {
+				fragments.dispose();
+			};
+			const disposeButton = new OBC.Button(components);
+			disposeButton.materialIcon = "delete";
+			toolbar.addChild(disposeButton);
+			disposeButton.onClick.add(() => disposeFragments());
+
+			const importExternalFragment = () => {
+				if (fragments.groups.length) return;
+				const input = document.createElement("input");
+				input.type = "file";
+				input.onchange = async () => {
+					if (input.files && input.files.length > 0) {
+						const file = input.files[0];
+						if (file.name.includes(".frag")) {
+							const url = URL.createObjectURL(file);
+							const result = await fetch(url);
+							const data = await result.arrayBuffer();
+							const buffer = new Uint8Array(data);
+							fragments.load(buffer);
+						}
+					}
+					input.remove();
+					if (!input.files || input.files.length === 0) {
+						input.click();
+					}
+				};
+				input.click();
+			};
+
+			const openButton = new OBC.Button(components);
+			openButton.materialIcon = "folder_open";
+			toolbar.addChild(openButton);
+			openButton.onClick.add(() => importExternalFragment());
+
 			const mainToolbar = new OBC.Toolbar(components, { name: "Main Toolbar", position: "bottom" });
-			mainToolbar.addChild(dimensions.uiElement.get("main"));
 			components.ui.addToolbar(mainToolbar);
+			const ifcButton = fragmentIfcLoader.uiElement.get("main") as OBC.Button;
+			console.log(ifcButton.name);
+			mainToolbar.addChild(ifcButton);
+
+			fragmentIfcLoader.settings.wasm = {
+				path: "https://unpkg.com/web-ifc@0.0.46/",
+				absolute: true,
+			};
+
+			const excludedCats = [WEBIFC.IFCTENDONANCHOR, WEBIFC.IFCREINFORCINGBAR, WEBIFC.IFCREINFORCINGELEMENT];
+			for (const cat of excludedCats) {
+				fragmentIfcLoader.settings.excludedCategories.add(cat);
+			}
+
+			fragmentIfcLoader.settings.webIfc.COORDINATE_TO_ORIGIN = true;
+			fragmentIfcLoader.settings.webIfc.OPTIMIZE_PROFILES = true;
+
+			// const loadIfcAsFragments = async () => {
+			// 	const file = await fetch("sample01.ifc");
+			// 	const data = await file.arrayBuffer();
+			// 	const buffer = new Uint8Array(data);
+			// 	const model = await fragmentIfcLoader.load(buffer, "example");
+			// 	scene.add(model);
+			// };
 		}
 	}
 };
