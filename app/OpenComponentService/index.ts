7b5487467221b3810f01f6158e8f23bb97f6b6b1
cbptamtom <cbptamcode@gmail.com>
2024-01-16 22:02:50 +0700

    learning tutorial

--- app/OpenComponentService/index.ts
+++ app/OpenComponentService/index.ts
@@ -5,7 +5,8 @@ import * as path from "path";
 import * as WEBIFC from "web-ifc";
 
 import Stats from "stats.js";
-import { importExternalFragment } from "./obc-services";
+import { importExternalFragment, loadIfcAsFragments } from "./obc-services";
+import { FragmentsGroup } from "bim-fragment";
 
 const openComponent = async (containerRef: RefObject<HTMLDivElement> | null) => {
 	if (containerRef && containerRef.current) {
@@ -33,14 +34,55 @@ const openComponent = async (containerRef: RefObject<HTMLDivElement> | null) => 
 			const grid = new OBC.SimpleGrid(components, new THREE.Color(0x666666));
 			components.tools.add("2fd526fe-c428-49c3-9c51-f02707d9a46c", grid);
 
-			const toolbar = new OBC.Toolbar(components);
-			components.ui.addToolbar(toolbar);
-			const loadButton = new OBC.Button(components);
-			loadButton.materialIcon = "Load Model";
-			toolbar.addChild(loadButton);
-			loadButton.onClick.add(() => importExternalFragment(fragments));
+			//#region Load IFC
+			const fragmentIfcLoader = new OBC.FragmentIfcLoader(components);
 
-			//I want to need make new tool - highlight element when select
+			const mainToolbar = new OBC.Toolbar(components, { name: "Main Toolbar", position: "bottom" });
+			components.ui.addToolbar(mainToolbar);
+			const ifcButton = fragmentIfcLoader.uiElement.get("main") as OBC.Button;
+			mainToolbar.addChild(ifcButton);
+			ifcButton.onClick.add(() => loadIfcAsFragments(fragmentIfcLoader, scene));
+
+			fragmentIfcLoader.settings.wasm = {
+				path: "https://unpkg.com/web-ifc@0.0.46/",
+				absolute: true,
+			};
+
+			const excludedCats = [WEBIFC.IFCTENDONANCHOR, WEBIFC.IFCREINFORCINGBAR, WEBIFC.IFCREINFORCINGELEMENT];
+			for (const cat of excludedCats) {
+				fragmentIfcLoader.settings.excludedCategories.add(cat);
+			}
+
+			fragmentIfcLoader.settings.webIfc.COORDINATE_TO_ORIGIN = true;
+			fragmentIfcLoader.settings.webIfc.OPTIMIZE_PROFILES = true;
+			const exploder = new OBC.FragmentExploder(components);
+			//#endregion
+			const toolbar = new OBC.Toolbar(components, {
+				name: "mainToolbar",
+				position: "bottom",
+			});
+			components.ui.addToolbar(toolbar);
+			const loadButton = new OBC.Button(components, {
+				materialIconName: "upload",
+				tooltip: "Load model",
+			});
+			toolbar.addChild(loadButton);
+			const culler = new OBC.ScreenCuller(components);
+			loadButton.onClick.add(async () => {
+				const model = (await importExternalFragment(fragments)) as FragmentsGroup;
+				if (model) {
+					console.log(model);
+					// const classifier = components.tools.get(OBC.FragmentClassifier);
+					// const properties = await fetch("small.json");
+					// model.properties = await properties.json();
+					// classifier.byStorey(model);
+					// for (const fragment of model.items) {
+					// 	culler.add(fragment.mesh);
+					// }
+				}
+			});
+
+			//#region Highlight elem
 			const highlighter = new OBC.FragmentHighlighter(components);
 			highlighter.outlineEnabled = true;
 			(components.renderer as OBC.PostproductionRenderer).postproduction.enabled = true;
@@ -53,10 +95,43 @@ const openComponent = async (containerRef: RefObject<HTMLDivElement> | null) => 
 				}
 				await highlighter.highlight("mySelect", true, false);
 			};
-
 			containerRef?.current.addEventListener("click", async () => {
 				await highlightElementOnClick();
 			});
+			//#endregion
+
+			//#region Export to fragment file
+			const exportFragments = () => {
+				if (!fragments.groups.length) return;
+				const group = fragments.groups[0];
+				const data = fragments.export(group);
+				const blob = new Blob([data]);
+				const file = new File([blob], "small.frag");
+				download(file);
+			};
+
+			const download = (file: File) => {
+				const link = document.createElement("a");
+				link.href = URL.createObjectURL(file);
+				link.download = file.name;
+				document.body.appendChild(link);
+				link.click();
+				link.remove();
+			};
+
+			const exportButton = new OBC.Button(components);
+			exportButton.materialIcon = "exit_to_app";
+			exportButton.tooltip = "Export model";
+			toolbar.addChild(exportButton);
+			exportButton.onClick.add(() => exportFragments());
+			//#endregion
+
+			//#region explore model => callback under
+
+			containerRef.current?.addEventListener("mouseup", () => (culler.needsUpdate = true));
+			containerRef.current?.addEventListener("wheel", () => (culler.needsUpdate = true));
+			culler.needsUpdate = true;
+			//#endregion
 		}
 	}
 };
--- app/OpenComponentService/obc-services.ts
+++ app/OpenComponentService/obc-services.ts
@@ -1,26 +1,53 @@
 import THREE from "three";
 
-export const importExternalFragment = (fragments: any) => {
+export const importExternalFragment = async (fragments: any) => {
 	if (fragments.groups.length) return;
+
+	return new Promise((resolve, reject) => {
+		const input = document.createElement("input");
+		input.type = "file";
+		input.onchange = async () => {
+			if (input.files && input.files.length > 0) {
+				const file = input.files[0];
+				if (file.name.includes(".frag")) {
+					const url = URL.createObjectURL(file);
+					try {
+						const result = await fetch(url);
+						const data = await result.arrayBuffer();
+						const buffer = new Uint8Array(data);
+						const model = fragments.load(buffer);
+
+						// Resolve the promise with the loaded model
+						resolve(model);
+					} catch (error) {
+						// Reject the promise if any error occurs
+						reject(error);
+					}
+				}
+			}
+			input.remove();
+
+			if (!input.files || input.files.length === 0) {
+				input.click();
+			}
+		};
+		input.click();
+	});
+};
+
+export const loadIfcAsFragments = async (fragmentIfcLoader: any, scene: any) => {
 	const input = document.createElement("input");
 	input.type = "file";
 	input.onchange = async () => {
 		if (input.files && input.files.length > 0) {
 			const file = input.files[0];
-			if (file.name.includes(".frag")) {
+			if (file.name.includes(".ifc")) {
 				const url = URL.createObjectURL(file);
 				const result = await fetch(url);
 				const data = await result.arrayBuffer();
 				const buffer = new Uint8Array(data);
-				fragments.load(buffer);
+				scene.add(buffer);
 			}
 		}
-		input.remove();
-		if (!input.files || input.files.length === 0) {
-			input.click();
-		}
 	};
-	input.click();
 };
-
-
