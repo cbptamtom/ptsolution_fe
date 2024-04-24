11be995bd76044cbd6588fdfa421eeeacc7275a5
cbptamtom <cbptamcode@gmail.com>
2024-04-24 22:31:04 +0700

    config some about section

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
-	return (
-		<main>
-			<Header />
-			<Hero />
-			<About />
-			<Blog />
-		</main>
-	);
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <About />
+      <Blog />
+    </main>
+  );
 }
